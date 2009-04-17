3813a90d005d1c3e12d95b95552e2049b838d492
wei <jianwei.gan@gmail.com>
2009-04-16 21:26:33 -0400

    added some parsing functionality, calls functions after parsing

--- emulator.cc
+++ emulator.cc
@@ -13,39 +13,66 @@ int stack[2*1024];
 int stack_pointer = 0x7fffefff;
 int registers[32];
 
-map<string, string> function_map;
-//for function map, arguments read from left to right as arg1, arg2, and arg3
-
 int mode;
 //mode 0 = run to completion
 //mode 1 = step through program
 
-
-function_map.put("add",
-		"arg1 = arg2 + arg3"
-);
-
-function_map.put ("addi",
-		"arg1 = arg2 + arg3"
-);
-
-function_map.put("and",
-		arg1 = arg2&arg3;
-);
-
-function_map.put("or",
-		arg1 = arg2|arg3;
-);
-
 void parseLine(int instruction) {
-
+	
+	// increment program pointer
+	pc += 4;
+	
 	//parse registry code
 	int opcode = (instruction & 0xFC000000) >> 26;
+	// R-type
+	int rs = (instruction & 0x3E00000) >> 21;
+	int rt = (instruction & 0x1F0000) >> 16;
+	int rd = (instruction & 0xF800) >> 11;
+	int shift = (instruction & 0x7C0) >> 6;
+	// I-type
+	int imm = instruction & 0xFFFF;
+	// J-type
+	int address = instruction & 0x3FFFFFF;
 	
 	switch (opcode) {
-		case 000000:
+		case 0x0:
+			// R type
+			int funct = instruction & 0x3F;
+			switch (funct):
+				case 0x20:
+					add(rd, rs, rt);
+				case 0x21:
+					addu(rd, rs, rt);
+				case 0x24:
+					commandAnd(rd, rs, rt); // need to call it something else
+				case 0x8:
+					jr(rs);
+				case 0x10:
+					mfhi(rs);
+				case 0x12:
+					mflo(rs);
+				case 0x18:
+					mult(rs, rt);
+				case 0x19:
+					multu(rs, rt);
+				case 0x25:
+					commandOr(rd, rs, rt);
+				case 0x0:
+					sll(rd, rt, shift);
+				case 0x15A:
+					slt(rd, rs, rt);
+				case 0x15B:
+					sltu(rd, rs, rt);
+					
+			
+		case 0x08: //addi
+			addi(rs, rt, imm);
+		case 0x09:
+			addiu(rs, rt, imm);
 		
-		
+			
+		default:
+			cout << "not a valid instruction" << endl;
 	}
 }
 
